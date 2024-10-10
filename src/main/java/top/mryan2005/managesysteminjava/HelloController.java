b145387ca3b1d29534c68e112ab384ac2b8182b1
Zhizhong Yan <A2564011261@163.com>
2024-10-10 16:35:54 +0800

    initial commit

---
+++ src/main/java/top/mryan2005/managesysteminjava/HelloController.java
@@ -0,0 +1,14 @@
+package top.mryan2005.managesysteminjava;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
