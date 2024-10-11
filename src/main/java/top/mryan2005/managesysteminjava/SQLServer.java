be3bb63cfe5b5bf7c5609dc9e279be112f6faf37
Zhizhong Yan <A2564011261@163.com>
2024-10-11 16:43:32 +0800

    fix some bugs

--- src/main/java/top/mryan2005/managesysteminjava/SQLServer.java
+++ src/main/java/top/mryan2005/managesysteminjava/SQLServer.java
@@ -4,17 +4,22 @@ import java.sql.*;
 import java.util.Properties;
 
 public class SQLServer {
-    private String username, password, databaseName, connectionUrl;
+    private String username, password, databaseName, ip, port, connectionUrl;
     private Connection con;
 
-    public SQLServer(String username, String password, String databaseName) throws SQLException {
-        connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName="+databaseName+";user="+username+";password="+password+";encrypt=false;";
+    public SQLServer(String ip, String port, String username, String password, String databaseName) throws SQLException {
+        this.username = username;
+        this.password = password;
+        this.databaseName = databaseName;
+        this.ip = ip;
+        this.port = port;
+        connectionUrl = "jdbc:sqlserver://"+ip+":"+port+";databaseName="+databaseName+";user="+username+";password="+password+";encrypt=false;";
         con = DriverManager.getConnection(connectionUrl);
     }
 
     public static void main(String args[]) throws SQLException {
         try {
-            SQLServer sql = new SQLServer("sa", "A123456", "abc");
+            SQLServer sql = new SQLServer("127.0.0.1", "1433", "sa", "A123456", "abc");
             System.out.println("连接成功！");
         } catch (SQLException e) {
             System.out.println("连接数据库时发生错误！");
