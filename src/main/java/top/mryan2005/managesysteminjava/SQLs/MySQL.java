75b1ec269a48623b6ec7235184734671ba1af1d0
Zhizhong Yan <A2564011261@163.com>
2024-10-12 08:23:05 +0800

    Link TO MySQL

--- src/main/java/top/mryan2005/managesysteminjava/SQLs/MySQL.java
+++ src/main/java/top/mryan2005/managesysteminjava/SQLs/MySQL.java
@@ -1,4 +1,36 @@
 package top.mryan2005.managesysteminjava.SQLs;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static java.sql.DriverManager.getConnection;
+
 public class MySQL {
+    private String ip, username, password, port, databaseName;
+    private Connection con;
+    public MySQL(String ip, String port, String username, String password, String databaseName) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        this.username = username;
+        this.password = password;
+        this.databaseName = databaseName;
+        this.ip = ip;
+        this.port = port;
+        String connectionUrl = "jdbc:mysql://"+ip+":"+port+"/"+databaseName+"?user="+username+"&password="+password+"&useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+        con = getConnection(connectionUrl);
+    }
+
+    public Connection getSQLer() {
+        return con;
+    }
+
+    public static void main(String args[]) throws SQLException, ClassNotFoundException {
+        try {
+            MySQL sql = new MySQL("100.67.158.142", "336", "root", "123456", "root");
+        } catch (SQLException e) {
+            System.out.println("连接数据库时发生错误！");
+            System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
