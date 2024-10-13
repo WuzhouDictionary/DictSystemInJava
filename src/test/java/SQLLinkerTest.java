acf25c6019a4523b847b00b31d4878ea5c10e69b
Zhizhong Yan <A2564011261@163.com>
2024-10-13 20:52:02 +0800

    fix some bugs

--- src/test/java/SQLLinkerTest.java
+++ src/test/java/SQLLinkerTest.java
@@ -1,11 +1,10 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import top.mryan2005.managesysteminjava.SQLs.MySQL;
 
 import static org.junit.Assert.assertTrue;
 
-public class TestSQLLinker {
-    @Test
-    public void testMethodOne() {
+public class SQLLinkerTest {
+    @Test public void testMethodOne() {
         MySQL sql = new MySQL();
         assertTrue(sql.testConnection("127.0.0.1", "3307", "root", "123456", "testDatabase"));
     }
