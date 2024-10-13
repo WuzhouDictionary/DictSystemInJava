b634cb2b52f5206080fca0061b5cf7abc545cfe0
Zhizhong Yan <A2564011261@163.com>
2024-10-13 20:21:39 +0800

    fix some bugs

--- src/test/java/TestSQLLinker.java
+++ src/test/java/TestSQLLinker.java
@@ -1,11 +1,11 @@
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
-import top.mryan2005.managesysteminjava.SQLs.MySQL;
+
+import static org.junit.Assert.assertTrue;
+import static top.mryan2005.managesysteminjava.SQLs.MySQL.testConnection;
 
 public class TestSQLLinker {
     @Test
     public void testMySQLLinker() {
-        assertTrue(MySQL.testConnection("127.0.0.1", "3307", "root", "123456", "testDatabase"));
+        assertTrue(testConnection("127.0.0.1", "3307", "root", "123456", "testDatabase"));
     }
 }
