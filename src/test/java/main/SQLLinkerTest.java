3a539ba5a7b1028b2b7fb11ea7e8e2e25f12ef87
Zhizhong Yan <A2564011261@163.com>
2024-10-14 17:00:45 +0800

    add test close connect task

--- src/test/java/main/SQLLinkerTest.java
+++ src/test/java/main/SQLLinkerTest.java
@@ -8,15 +8,15 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SQLLinkerTest {
-    public SQLLinker mysql;
     @Test
     public void TestConnectSQL() throws SQLException, ClassNotFoundException {
-        mysql = new SQLLinker("MySQL", "127.0.0.1", "3800", "root", "123456", "testDatabase");
+        SQLLinker mysql = new SQLLinker("MySQL", "127.0.0.1", "3800", "root", "123456", "testDatabase");
         assertTrue(mysql.testConnection());
     }
 
     @Test
     public void TestCloseConnect() throws SQLException, ClassNotFoundException {
+        SQLLinker mysql = new SQLLinker("MySQL", "127.0.0.1", "3800", "root", "123456", "testDatabase");
         assertTrue(mysql.closeConnection());
     }
 }
